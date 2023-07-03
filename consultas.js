d7a89d3d98b718f6743a64e355915ba44169ee37
Yago César <yagocdj@gmail.com>
2023-07-03 20:12:57 -0300

    nova consulta

--- consultas.js
+++ consultas.js
@@ -9,3 +9,35 @@ db.funcionario.find({idade: {$gte : 20},
 //- 1 consulta com pelo menos aggregate e lookup;
 db.projeto.find({})
 
+//- 1 consulta com pelo menos sort e limit e filtros e projeções;
+
+/*
+Exibir o nome e a data prevista de 2 projetos cuja data prevista seja maior do que 2020-01-01 e que
+um dos seus gêneros seja 'Mundo aberto'. Posteriormente, ordenar pela data prevista 
+*/
+
+// exemplo com ISODate
+db.projeto.find(
+    {
+        "dataPrevista": {$gte: ISODate("2020-01-01")},
+        "genero": "Mundo Aberto"
+    },
+    {
+        "nome": 1,
+        "dataPrevista": 1,
+        "_id": 0
+    }
+).sort({"dataPrevista": 1}).limit(2);
+
+// exemplo sem ISODate
+db.projeto.find(
+    {
+        "dataPrevista": {$gte: "2020-01-01"},
+        "genero": "Mundo Aberto"
+    },
+    {
+        "nome": 1,
+        "dataPrevista": 1,
+        "_id": 0
+    }
+).sort({"dataPrevista": 1}).limit(2);
